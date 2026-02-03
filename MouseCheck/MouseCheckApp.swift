d3257604c0a7eb397eed98f37b7d5294ac5393ff
vd <dulnev@gmail.com>
2026-02-03 22:06:14 +0200

    Initial Commit

---
+++ MouseCheck/MouseCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MouseCheckApp.swift
+//  MouseCheck
+//
+//  Created by vd on 03.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct MouseCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
